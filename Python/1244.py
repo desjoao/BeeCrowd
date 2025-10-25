6b9af0a3534be842b41d11ccf651b0f802da386c
desjoao <joaopedrochg@gmail.com>
2025-10-24 22:59:25 -0300

    adição de solução para o problema 1244

--- Python/1244.py
+++ Python/1244.py
@@ -1,17 +1,25 @@
-def ordena_palavras(string: str) -> str:
-    string = string.split(" ")
-    for i in range(0, len(string) -1):
-        for j in range(i, len(string)):
-            if len(string[i]) < len(string[j]):
-                aux = string[i]
-                string[i] = string[j]
-                string[j] = aux
-    saida = ""
-    for palavra in string:
-        saida+= palavra + " "
-    return saida
+def ordena_frase(entrada: str) -> list:
+    lista = list(map(str, entrada.split()))
+    for i in range(0, len(lista)-1):
+        maior = len(lista[i])
+        for j in range(len(lista)-1, i-1, -1):
+            if len(lista[j]) >= maior:
+                maior = len(lista[j])
+                ref = j
+        valor = lista.pop(ref)
+        lista.insert(i, valor)
+    return lista
 
-if __name__ == "__main__":
-    string = input()
-    string = ordena_palavras(string)
-    print(string)
+if __name__ == '__main__':
+    vezes = int(input())
+    lista_saida = []
+    for i in range(vezes):
+        lista = ordena_frase(input())
+        saida = ''
+        for palavra in lista:
+            saida+=palavra+' '
+        lista_saida.append(saida[:-1])
+    for frase in lista_saida:
+        print(frase)
+
+            
